a3eb212b5984f7f6d0b638f09d707f4ebe144356
Joao Rolo <roloje777@gmail.com>
2024-07-10 09:02:13 +0100

    add assignment 11

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,8 +1,13 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.services.BeerService;
+import guru.springframework.spring6restmvc.services.BeerServiceImpl;
 import guru.springframework.spring6restmvc.services.CustomerService;
 import guru.springframework.spring6restmvc.services.CustomerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,8 +16,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -24,7 +31,40 @@ class CustomerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+    @Autowired
+    ObjectMapper objectMapper; // Maps POJO to JSON
+
+    CustomerServiceImpl customerServiceImpl;
+
+    // runs before each test
+    @BeforeEach
+    void setUp(){
+      customerServiceImpl = new CustomerServiceImpl() ;
+    }
+
+    /*
+    Use Spring MockMVC and Mockito to test Create endpoint for Customer
+    Write test to test creation of customer
+    Update Test to re-create CustomerServiceImpl before each test
+    Verify HTTP 201 is returned
+    Verify Location Header is returned
+     */
+    @Test
+    void testCreateNewCustomer() throws Exception {
+        // post customer does not have these fields
+        Customer sentCustomer = customerServiceImpl.getAllCustomers().get(0);
+        sentCustomer.setId(null);
+        sentCustomer.setVersion(null);
+
+        given(customerService.saveNewCustomer(any(Customer.class))).willReturn(customerServiceImpl.getAllCustomers().get(1));
+
+        mockMvc.perform(post("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(sentCustomer))) // post object as JSON
+                .andExpect(status().isCreated()) // status 201
+                .andExpect(header().exists("Location"));// check if returned object has a location set
+    }
 
     @Test
     void listAllCustomers() throws Exception {
